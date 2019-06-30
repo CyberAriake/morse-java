f42b3b377a8d93dcab2a16f7bb98164aedb575fc
CyberAriake <taketomono.tomonari@icloud.com>
2019-06-30 11:46:16 +0900

    change

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,37 +2,86 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-    private boolean flag = false;
+    private SoundPool soundPool;
+    private int soundYa;
+    int streamId=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setAudioAttributes(audioAttributes)
+                .setMaxStreams(2)
+                .build();
+
+        soundYa = soundPool.load(this, R.raw.ya,1);
+
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Log.d("debug", "sampleId=" + sampleId);
+                Log.d("debug", "status=" + status);
+            }
+        });
+
+
         textView = findViewById(R.id.text_view);
 
         Button button = findViewById(R.id.button);
+        button.setOnTouchListener(new View.OnTouchListener() {
 
-        button.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view){
-                if(flag){
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    streamId = soundPool.play(soundYa,1.0f,1.0f,0,0,1);
                     textView.setText(R.string.hello);
-                    flag = false;
-                }
-                else{
+
+                    return true;
+                } else {
+                    soundPool.stop(streamId);
                     textView.setText(R.string.world);
-                    flag = true;
+                    return false;
                 }
             }
         });
     }
 }
+
+
+
+//        button.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                if(flag){
+//                    textView.setText(R.string.hello);
+//                    flag = false;
+//                }
+//                else{
+//                    textView.setText(R.string.world);
+//                    flag = true;
+//                }
+//            }
+//        });
+//    }
+//}
